6b5ba60f8ac2d864308fb38b57ebb6d43dff5228
Raquel Biondi <raquelsylos@gmail.com>
2023-09-04 12:34:48 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#btn-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoNovaImagem = $('#endereço-imagem-nova').val();
+        console.log(enderecoNovaImagem);
+        const novoItem = $('<li style="display:none"></li>');
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-image-link">
+                <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereço-imagem-nova').val('');
+    })
+});
+
